90f7991306f37c5d3442ec812464ecb7b678f110
Oleksandr Onufrienko <oonufrienko@cogniance.com>
2019-01-04 15:59:45 +0200

    Start and Stop work

--- js_05-06/js/script.js
+++ js_05-06/js/script.js
@@ -1,52 +1,56 @@
-var miliseconds = '000';
-var seconds = '00';
-var minutes = '00';
-var hours = '00';
+var timerDigits = document.querySelectorAll('span');
+var counter = 0;
 
-// debugger;
+function runTimer() {
+    // debugger;
+    counter++;
 
-var MAX_MILISECONDS = 999;
-var MAX_SECONDS = 59;
-var MAX_MINUTES = 59;
-var MAX_HOURS = 23;
+    var miliSec_num = parseInt(counter, 10);
+    
+    var hours   = Math.floor(miliSec_num / (3600 * 1000));
+    var minutes = Math.floor((miliSec_num - (hours * 3600 * 1000)) / 60000);
+    var seconds = Math.floor((miliSec_num - (hours * 3600 * 1000) - (minutes * 60000)) / 1000);
+    var miliseconds = miliSec_num - (hours * 3600 * 1000) - (minutes * 60000) - (seconds * 1000);
 
-// handler
-
-function miliSecondsCounter() {
-    console.log("counter run");
-    miliseconds++;
-
-    if (miliseconds >= MAX_MILISECONDS) {
-        clearInterval(timerId);
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    if (miliseconds < 10) {
+        miliseconds = "00" + miliseconds;
+    } else if (miliseconds < 100) {
+        miliseconds = "0" + miliseconds;
     }
+
+    resultHHMMSS = hours+':'+minutes+':'+seconds;
+    timerDigits[0].innerHTML = resultHHMMSS;
+    timerDigits[1].innerHTML = miliseconds;
+    
+    // return result;
+
 }
 
-// miliseconds 
-function transformCounter(counterValue) {
-
-    if (counterValue <= 9) {
-        return '00' + counterValue;
-    } else if (counterValue <= 99) {
-        return '0' + counterValue;
-    } else {
-        return '' + counterValue;
-    }
+function handler(e) {
+    setInterval(runTimer, 1);
 }
 
 function buttonHandler(event) {
     if (event.target.innerHTML === "Pause") {
         event.target.innerHTML = "Go on";
+        timerId = setInterval(runTimer, 1);
         //start timer
     } else if (event.target.innerHTML !== "Stop" ) {
         event.target.innerHTML = "Pause";
         // start timer
+        timerId = setInterval(runTimer, 1);
     } else {
         btnStart[0].innerHTML = "Start"
         // reset all the counters
+        clearInterval(timerId);
+        counter = 0;
+        timerDigits[0].innerHTML = '00:00:00';
+        timerDigits[1].innerHTML = '000';
     }
 }
-
-// var timerId = setInterval(miliSecondsCounter, 1);
 
 var btnStart = document.getElementsByClassName('btn btn-outline-primary');
 var btnStop = document.getElementsByClassName('btn btn-outline-danger');
