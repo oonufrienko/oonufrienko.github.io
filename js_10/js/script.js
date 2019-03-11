00320327a931ab14ffa9468ad9758356ac7ba178
Oleksandr Onufrienko <oonufrienko@cogniance.com>
2019-03-11 13:32:23 +0200

    Fully re-create menu. Almost finished.

--- js_10/js/script.js
+++ js_10/js/script.js
@@ -1,28 +1,68 @@
-$(function(){
-   
-    // 
-    var something = $('li.has-submenu').hover( function() {
+$(function (){
 
-        $('ul.submenu > li').slideDown("fast");
-        $(this).children().toggleClass('changed');
-        // debugger;
-        // var a = $(this);
-        // .children()
-        // $(this).off('mouseleave');
-        // 
-        var domElement = $( this ).get(0);
-        $( "span:first" ).text( "Hovered on - " + domElement.nodeName );
-
-    }, function() {
-    
-        // var t = $(this);
-        // debugger;
-        var domElement = $( this ).get(0);
-        $( "span:first" ).text( "Hovered out - " + domElement.nodeName );
-        $('ul.submenu > li').slideUp("fast");
-        $(this).children().toggleClass('changed');
-        // children().
+    $(".has-sub").hover(function(e){
+      $(".has-sub > ul").slideDown("slow", function(){
+  
+        var numberOfSubmenuItems;
+        var $this;
+        var isMenuOpened = false;
+  
+        $('ul.sub-menu-level1').on({
+              'mouseenter': function(){
+                $this = $(this);
+                var ch = $this.children();
+                numberOfSubmenuItems = $this.children().length;
+                var top = 0;
+                var px = 39;
+               
+                console.log("1 MouseEnter Check if menu has already opened, isMenuOpened = " + isMenuOpened);
+  
+                // check if menu has already opened
+                // if ( !isMenuOpened ) {
+                  // debugger;
+                  // check if there is next level of menu 
+                      isMenuOpened = true;
+  
+                    if ( numberOfSubmenuItems > 0 ) {
+  
+                      top = $this.index() * px + "px";
+  
+                      console.log("2 MouseEnter slideDown before, isMenuOpened = " + isMenuOpened);
+  
+                      $("ul.sub-menu-level2").eq($this.index()).css("top", top).slideDown("slow", function(){
+                        isMenuOpened = false;
+                        console.log("3 MouseEnter slideDown inside, isMenuOpened = " + isMenuOpened);
+                      });
+  
+                      console.log("4 MouseEnter slideDown after, isMenuOpened = " + isMenuOpened);
+                    // $("span").text($this.index());
+                    }
+                  // } 
+                }
+              }, 'li' );
+  
+        $('ul.sub-menu-level1').on({
+              'mouseleave': function(){
+                // debugger;
+  
+                console.log("1 MouseLeave. Just inside. Check isMenuOpened = " + isMenuOpened);
+  
+              // if ( !isMenuOpened ) {
+                isMenuOpened = true;
+                console.log("2 MouseLeave. slideUp before, isMenuOpened = " + isMenuOpened);
+                $("ul.sub-menu-level2").eq($this.index()).slideUp("slow", function(){
+                  isMenuOpened = false;
+                  console.log("3 MouseLeave. slideUp inside, isMenuOpened = " + isMenuOpened);
+                });
+              // }
+              console.log("4 MouseLeave. slideUp after, isMenuOpened = " + isMenuOpened);
+                  }
+              }, 'li' );
+  
     });
-    var i = 7;
-
-});
+      
+    }, function() {
+      $(".has-sub > ul").slideUp("slow");
+    });
+  
+  });
