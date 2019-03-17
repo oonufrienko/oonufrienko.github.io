68d870ed1ecbca66cfae0cfbf25c0c83a278d8e7
Oleksandr Onufrienko <oonufrienko@cogniance.com>
2019-03-17 09:03:47 +0200

    update js-07

--- js_07/js/script.js
+++ js_07/js/script.js
@@ -1,23 +1,19 @@
 $(function() {
 
-    var $tabs = $('.tabs ul li');
-    $('.tabs ul li:first').addClass('active');
+  var $tabs = $('.tabs ul li');
+  var $tabContent = $('div.content');
+  $tabs.eq(0).addClass('active');
+  $tabContent.eq(0).addClass('active');
 
-    $tabs.on('click', function(e){
-        var $tabItem = $('.active');
-        $tabItem.removeClass( "active" );
+  $tabs.on('click', function(e){
+    $tabs.removeClass('active');
+    $tabContent.removeClass('active');
 
-        var $tabName = $(this).text();
+    var activeNumber = $tabs.index($(this));
 
-        if ( $tabName === "Zebras") {
-            $('div.content-zebra').addClass('active');
-        } else if ( $tabName === "Elephant") {
-            $('div.content-elephant').addClass('active');
-        } else {
-            $('div.content-rhino').addClass('active');
-        }
-
-        $(this).addClass('active');
+    $tabs.eq(activeNumber).addClass('active');
+    $tabContent.eq(activeNumber).addClass('active');
+       
 
     });
 
