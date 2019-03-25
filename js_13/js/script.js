f1d210c022e53838bc6d0683f767b3ebdf49636a
Oleksandr Onufrienko <oonufrienko@cogniance.com>
2019-03-25 15:22:58 +0200

    add logic how to define chosen items

--- js_13/js/script.js
+++ js_13/js/script.js
@@ -13,8 +13,49 @@ $(function(){
     var content = tmpl(sectionQA);
 
     $('.content').append(content);
-  }
 
-localStorage.clear();
+    $('.btn').on('click', function(){
+      debugger;
+      // read the correct answers from the localStorage
+        var correctAnswers = JSON.parse(localStorage.getItem('correctAnswers'));
+        var n = 3;
+        var sectionNumber = -1;
+        var correctAnswer = -1;
+        var result = {
+          0: undefined,
+          1: undefined,
+          2: undefined
+        };
+
+        $( '.answers li input' ).each(function( index ) {
+          if ($(this).prop('checked')) {
+            debugger;
+            console.log( 'index =', index );
+            sectionNumber = parseInt(index / n);
+            correctAnswer = index - (sectionNumber * n);
+            if (correctAnswers.answerId[sectionNumber] === correctAnswer) {
+              result[sectionNumber] = true;
+            } else {
+              result[sectionNumber] = false;
+            }
+          }
+        });
+
+        showModal(result);
+
+    });
+
+
+    function showModal() {
+      
+    }
+
+}
+
+
+
+
+
+// localStorage.clear();
 
 });
