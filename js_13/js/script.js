4f84dc49a46cdb4cb97a6c9185f25887bfe102ab
Oleksandr Onufrienko <oonufrienko@cogniance.com>
2019-05-07 12:02:25 +0300

    Move part of code to method

--- js_13/js/script.js
+++ js_13/js/script.js
@@ -5,58 +5,54 @@ $(function(){
   if (!localStorage.getItem('questions')) { return; }
 
     var questions = JSON.parse(localStorage.getItem('questions'));
-
     var html = $('#content-template').html();
-
     var tmpl = _.template(html);
-
     var content = tmpl(questions);
     
     $('.content').append(content);
     
-    $('.btn').on('click', function(){
-      var sectionNumber = -1;
-      var answerNumber = -1;
-      var results = {
-        id: [ 'вы не отметили', 'вы не отметили', 'вы не отметили' ]
-      };
-
-      $( '.answers li input' ).each(function( index ) {
-        if ($(this).prop('checked')) {
-          sectionNumber = parseInt(index / questions.length);
-          answerNumber = index - (sectionNumber * questions.length);
-
-          if (questions[sectionNumber].answers[answerNumber].isCorrect) {
-            results.id[sectionNumber] = 'верно';
-          } else {
-              results.id[sectionNumber] = 'не верно';
-          }
-
-        }
-      });
-
-      showModal(results);
+    $('.btn').on('click', function(){      
+      showModal(defineResults('.answers li input'));
 
       $('.result-modal .btn-close').on('click', function () {
         hideModal();
       });
-
+      
     });
+    
+    function defineResults(listElement) {
+      var sectionNumber = -1;
+      var answerIndex = -1;
+      var results = {
+        id: [ 'вы не отметили', 'вы не отметили', 'вы не отметили' ]
+      };
+  
+      $( listElement ).each(function( liIndex ) {
+        if ($(this).prop('checked')) {
+          sectionNumber = parseInt(liIndex / questions.length);
+          answerIndex = liIndex - (sectionNumber * questions.length);
+  
+          if (questions[sectionNumber].answers[answerIndex].isCorrect) {
+            results.id[sectionNumber] = 'верно';
+          } else {
+              results.id[sectionNumber] = 'не верно';
+          }
+  
+        }
+      });
 
+      return results;
+    }
 
     function showModal(result_obj) {
-
       $('.content').after('<div class="overlay"></div>');
       $('.overlay').after('<div class="result-modal"></div>');
 
       var htmlAnswerTemplate = $('#answers').html();
-
       var tmplFunction = _.template(htmlAnswerTemplate);
-
       var resultModalHTML = tmplFunction(result_obj);
 
       $('.result-modal').append(resultModalHTML);
-
       $('.result-modal').show();
 
     }
@@ -64,7 +60,7 @@ $(function(){
     function hideModal() {
       $('div').remove('.overlay');
       $('div').remove('.result-modal');
-      $( '.answers li input' ).each(function( index ) {
+      $( '.answers li input' ).each(function( liIndex ) {
         if ($(this).prop('checked')) {
           $(this).prop('checked', false);
         }
