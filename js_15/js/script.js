b421ef6d0922d2cc5a3ac845e7398af7b32a04bc
Oleksandr Onufrienko <oonufrienko@cogniance.com>
2019-06-07 15:47:19 +0300

    add Enter pressing handling

--- js_15/js/script.js
+++ js_15/js/script.js
@@ -2,7 +2,28 @@
 
 $(function() {
 
-  $('.btn-search').on('click', function() {
+  function renderData( data ) {
+    if (parseInt(data.totalHits) > 0) {
+      var results = {
+        items: []
+      };
+      $.each(data.hits, function( i, hit ) { 
+        results.items.push( "<img src='" + hit.previewURL + "'>" );
+      });
+      var html = $('#content-template').html();
+      var tmpl = _.template(html);
+      var content = tmpl(results);
+      
+      $('.results-title').show();
+      $('.search-results').append(content);
+    }
+    else {
+      $('.results-title').show();
+      $('.results-title').after('<p>No hits</p>');
+    }
+  }
+
+  function queryHandler() {
     var queryString = $('.query-field').val();
     $('.results-title').hide();
     $('p').remove(":contains('No hits')");
@@ -16,32 +37,19 @@ $(function() {
       return; 
     }
       
-      var URL = 'https://pixabay.com/api?key=12682041-c02212121d8f52a952705a821&q=' + queryString + '&image_type=photo';
+    var URL = 'https://pixabay.com/api?key=12682041-c02212121d8f52a952705a821&q=' + queryString + '&image_type=photo';
 
-      $.ajax({
+    $.ajax({
         url: URL
       })
-      .done(function( data ) {
-        if (parseInt(data.totalHits) > 0) {
-          var results = {
-            items: []
-          };
-          $.each(data.hits, function( i, hit ) { 
-            results.items.push( "<img src='" + hit.previewURL + "'>" );
-          });
-          var html = $('#content-template').html();
-          var tmpl = _.template(html);
-          var content = tmpl(results);
-          
-          $('.results-title').show();
-          $('.search-results').append(content);
-        }
-        else {
-          $('.results-title').show();
-          $('.results-title').after('<p>No hits</p>');
-        }
-      });
-    
-    });
+      .done( function (data) {renderData(data);} );
+  }
+
+  $('.btn-search').on('click', queryHandler);
+  $('.query-field').on('keypress', function(e) {
+    if(e.which === 13) {
+      queryHandler();
+    }
+});
 
 });
