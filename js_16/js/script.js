7ec4ff0a1aa7f8ec825e2daed3d7890526742c14
Oleksandr Onufrienko <oonufrienko@cogniance.com>
2019-06-09 22:06:23 +0300

    added verification for empty string enterance

--- js_16/js/script.js
+++ js_16/js/script.js
@@ -1,17 +1,38 @@
 'use strict'
 
 function loadData() {
-  // clear the result section. show/hide Result word
+  // Begin. Clear the result section. show/hide Result word
   var ul = document.getElementsByTagName('ul')[0];
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
   }
-  //  I think that's solution is not optimize. Optimize solution is to add <ul> section in renderData function, then remove it. Not in loop but at once if it exists.
-  var xhr = new XMLHttpRequest();
+  // The solution isn't optimize. 
+  // Optimize solution is to add <ul> section in renderData function, then remove it here. 
+  // Not in loop but at once if it exists.
+
+  var resultWord = document.getElementsByClassName('results-title')[0];  
+  resultWord.style.display = 'none';
+  if ( resultWord.firstElementChild ) {
+    resultWord.removeChild(resultWord.firstElementChild);
+  }
+  // End of clearing result section 
+
   var queryString = document.getElementsByClassName('query-field')[0].value;
 
-  if ( !queryString ) { return; }
+  if ( !queryString ) { 
+    var resultWord = document.getElementsByClassName('results-title')[0];
+    resultWord.style.display = 'block';
 
+    if ( !resultWord.firstElementChild ) {
+      var resultNoHits = document.createElement('p');
+      resultNoHits.innerHTML = 'No hits'
+      resultWord.appendChild(resultNoHits);
+    }
+
+    return; 
+  }
+
+  var xhr = new XMLHttpRequest();
   var URL = 'https://pixabay.com/api?key=12682041-c02212121d8f52a952705a821&q=' + queryString + '&image_type=photo';
 
   xhr.open('GET', URL);
@@ -39,9 +60,12 @@ function renderData(items) {
     img.src = item.previewURL;
     li.appendChild(img);
   });
+  var resultWord = document.getElementsByClassName('results-title')[0];
+  resultWord.style.display = 'block';
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+
   var searchButton = document.getElementsByClassName('btn-search')[0];
   searchButton.addEventListener('click', loadData);
 
