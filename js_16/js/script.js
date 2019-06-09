c42a3b94b018f4eed359a788d985ce5f1a115d5e
Oleksandr Onufrienko <oonufrienko@cogniance.com>
2019-06-09 15:47:53 +0300

    add task with XMLHttpRequest

---
+++ js_16/js/script.js
@@ -0,0 +1,54 @@
+'use strict'
+
+function loadData() {
+  // clear the result section
+  
+  //   
+  var xhr = new XMLHttpRequest();
+  var queryString = document.getElementsByClassName('query-field')[0].value;
+
+  if ( !queryString ) { return; }
+
+  var URL = 'https://pixabay.com/api?key=12682041-c02212121d8f52a952705a821&q=' + queryString + '&image_type=photo';
+
+  xhr.open('GET', URL);
+  xhr.send();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) { return; }
+
+    if (xhr.status != 200) {
+      // show the error
+      alert( xhr.status + ': ' + xhr.statusText );
+    } else {
+        var items = JSON.parse(xhr.responseText);
+        renderData(items);
+    }
+
+  }
+
+}
+
+function renderData(items) {
+  var resultSection = document.getElementsByClassName('search-results')[0];
+  var list = resultSection.getElementsByTagName('ul')[0];
+
+  items.hits.forEach(function(item) {
+    var li = list.appendChild(document.createElement('li'));
+    var img = document.createElement('img');
+    img.src = item.previewURL;
+    li.appendChild(img);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var searchButton = document.getElementsByClassName('btn-search')[0];
+  searchButton.addEventListener('click', loadData);
+
+  var searchField = document.getElementsByClassName('query-field')[0];
+  searchField.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        loadData();
+    }
+  });
+
+});
